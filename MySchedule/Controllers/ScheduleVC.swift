981934787b91aee056fa64ba3d828d23dfc53094
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-05-09 23:44:20 +0400

    add swipes on calendar

--- MySchedule/Controllers/ScheduleVC.swift
+++ MySchedule/Controllers/ScheduleVC.swift
@@ -6,12 +6,115 @@
 //
 
 import UIKit
+import FSCalendar
 
 class ScheduleVC: UIViewController {
+    
+    private var calendarHideConstraint : NSLayoutConstraint!
+    
+    private var calendar : FSCalendar = {
+        let calendar = FSCalendar()
+        
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    private let showHideButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show calendar", for: .normal)
+        button.setTitleColor(.systemGray, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         title = "Schedule"
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        calendar.scope = .week
+        
+        setConstraints()
+        swipeAction()
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func showHideButtonTapped() {
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Hide calendar", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Show calendar", for: .normal)
+        }
+    }
+    
+    //MARK: - swipe gesture recognizer
+    func swipeAction() {
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        calendar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case .up:
+           showHideButtonTapped()
+        case .down:
+           showHideButtonTapped()
+        default:
+            break
+        }
+    }
+}
+
+//MARK: - FSCalendarDataSource, FSCalendarDelegate
+extension ScheduleVC: FSCalendarDataSource, FSCalendarDelegate {
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHideConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
+
+//MARK: - setConstraints
+extension ScheduleVC {
+    func setConstraints() {
+        
+        view.addSubview(calendar)
+        
+        //MARK: - config calendarHideConstraint
+        calendarHideConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHideConstraint)
+        
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        
+        //MARK: - config for showHideButton
+        view.addSubview(showHideButton)
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            showHideButton.widthAnchor.constraint(equalToConstant: 100),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
 }
