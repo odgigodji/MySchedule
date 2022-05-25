4b775a70aeee9b98ccbd640cca54409797213d3b
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-05-25 14:14:51 +0400

    coolers added on ColorsVC

--- MySchedule/Model/Cell/Schedule/ColorScheduleTableViewCell.swift
+++ MySchedule/Model/Cell/Schedule/ColorScheduleTableViewCell.swift
@@ -31,7 +31,17 @@ class ColorScheduleTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+   //["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "DEEP BLUE", "PURPLE"]
     func configure(indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0: backgroundViewCell.backgroundColor = #colorLiteral(red: 0.9294223189, green: 0.3272274435, blue: 0.4104554057, alpha: 1)
+        case 1: backgroundViewCell.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        case 2: backgroundViewCell.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        case 3: backgroundViewCell.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        case 4: backgroundViewCell.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        case 5: backgroundViewCell.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        default: backgroundViewCell.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        }
     }
     
         
