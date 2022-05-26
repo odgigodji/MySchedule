c49e149098f57fa697b0a63d63601f08172f7671
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-05-26 20:49:22 +0400

    constraints for contactImageView

--- MySchedule/Model/Cell/Contacts/ContactsTableViewCell.swift
+++ MySchedule/Model/Cell/Contacts/ContactsTableViewCell.swift
@@ -18,7 +18,29 @@ class ContactsTableViewCell: UITableViewCell {
         return imageView
     }()
     
+    let phoneImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "phone.fill")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
+    let mailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "envelope.fill")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let nameLabel = UILabel(text: "Dude", font: .avenirNext20())
+    let phoneLabel = UILabel(text: "+79205104201", font: .avenirNext14())
+    let mailLabel = UILabel(text: "Dude@gmail.com", font: .avenirNext14())
+    
+    override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        
+        contactImageView.layer.cornerRadius = contactImageView.frame.height / 2
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,6 +57,12 @@ class ContactsTableViewCell: UITableViewCell {
     
     //MARK: - Set constraints for subviews on cell
     func setConstrains() {
-        
+        self.addSubview(contactImageView)
+        NSLayoutConstraint.activate([
+            contactImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+            contactImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+            contactImageView.widthAnchor.constraint(equalToConstant: 70),
+            contactImageView.heightAnchor.constraint(equalToConstant: 70)
+        ])
     }
 }
