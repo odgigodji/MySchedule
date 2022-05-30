41222aff3158e6cbe8c602195ac2b58176ddb4e8
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-05-30 19:55:13 +0400

    RealmManager

--- MySchedule/Services/RealmManager.swift
+++ MySchedule/Services/RealmManager.swift
@@ -7,3 +7,16 @@
 
 import RealmSwift
 
+class RealmManager {
+    static let shared = RealmManager()
+    
+    private init() {}
+    
+    let localRealm = try! Realm()
+    
+    func saveScheduleModule(model: ScheduleModel) {
+        try! localRealm.write {
+            localRealm.add(model)
+        }
+    }
+}
