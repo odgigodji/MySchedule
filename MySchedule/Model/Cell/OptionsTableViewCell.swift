c36afc0a2c93ba5cb5a11982854407a6c96c9dde
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-06-01 19:51:15 +0400

    switch color not changing now

--- MySchedule/Model/Cell/OptionsTableViewCell.swift
+++ MySchedule/Model/Cell/OptionsTableViewCell.swift
@@ -30,7 +30,7 @@ class OptionsTableViewCell: UITableViewCell {
         let repeatSwitch = UISwitch()
         repeatSwitch.isOn = true
         repeatSwitch.isHidden = true
-//        repeatSwitch.onTintColor = .init(red: 0.3, green: 0.2 , blue: 0.8, alpha: 1.0)
+        repeatSwitch.onTintColor = UIColor().colorFromHex("1A4766")
         repeatSwitch.translatesAutoresizingMaskIntoConstraints = false
         return repeatSwitch
     }()
@@ -58,7 +58,7 @@ class OptionsTableViewCell: UITableViewCell {
         
         backgroundViewCell.backgroundColor = (indexPath.section == 3 ? color : .white)
         repeatSwitch.isHidden = (indexPath.section == 4 ? false : true)
-        repeatSwitch.onTintColor = color
+//        repeatSwitch.onTintColor = color
     }
     
     func tasksConfigure(nameArray: [String], indexPath: IndexPath) {
